2eae2e532ca4f8700efc04b00b3ab6af94197821
chemoune <chemounetorres@yahoo.fr>
2017-12-04 10:47:54 +0100

    Controller/

---
+++ Labyrinthe/src/Controller/Controller.java
@@ -0,0 +1,31 @@
+package Controller;
+
+import javafx.stage.Stage;
+import View.View;
+import Model.*;
+
+public class Controller {
+
+	private static Controller instance = new Controller();
+	private View view;
+	private Labyrinthe laby;
+
+	private Controller() {
+		laby = Labyrinthe.getInstance();
+		view = View.getInstance();
+		laby.GetExit().startPosition();
+		Vertex v = laby.GetExit().getPosition();
+		laby.getG().addVertex(v);
+		laby.buildRandomPath(v);
+		laby.getPackman().startPosition(laby, laby.getG().getEqualVertex(v));
+	}
+
+	public static Controller getInstance() {
+		return instance;
+	}
+
+	public void start(Stage primaryStage) throws Exception {
+		view.start(primaryStage);
+	};
+
+}
